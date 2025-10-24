f8c010880ac0fb8802c3a67f783bd4c848651ca8
itu61 <lit.sbz@outlook.com>
2025-10-25 02:02:40 +0900

    [chore] 使わなくなったAppProviderを消す

--- src/main.tsx
+++ src/main.tsx
@@ -5,19 +5,15 @@ import './index.css'
 import MonitorPage from './pages/monitor.tsx'
 import LoginPage from './pages/login.tsx'
 import WarningPage from "./pages/warning.tsx";
-import {AppProvider} from "./AppContext.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <BrowserRouter>
-          <AppProvider>
           <Routes>
               <Route path={"/id-list"} element={<MonitorPage />} />
               <Route path={"/login"} element={<LoginPage />} />
               <Route path={"/warning"} element={<WarningPage />} />
           </Routes>
-          </AppProvider>
       </BrowserRouter>
-
   </StrictMode>
 )
